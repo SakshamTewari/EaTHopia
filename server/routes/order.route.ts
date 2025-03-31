5c73ab53c47e70bbc1e24458aa29849457d222b0
Saksham Tewari <sakshamtewari3@gmail.com>
2025-03-31 23:33:24 +0530

    'Order' routes

---
+++ server/routes/order.route.ts
@@ -0,0 +1,12 @@
+import express from 'express';
+import { isAuthenticated } from '../middleware/isAuthenticated';
+import {
+  getRestaurantOrders,
+  updateOrderStatus,
+} from '../controller/order.controller';
+
+const router = express.Router();
+
+router.route('/restaurant/orders').get(isAuthenticated, getRestaurantOrders);
+
+router.route('/:orderId/status').put(isAuthenticated, updateOrderStatus);
